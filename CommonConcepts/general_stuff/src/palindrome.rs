a27801a89cc23b9a27bbb71233f1f84534447cbf
retrymp3 <retrymp492@gmail.com>
2023-06-21 00:11:52 +0530

    removed binaries

--- CommonConcepts/general_stuff/src/palindrome.rs
+++ CommonConcepts/general_stuff/src/palindrome.rs
@@ -3,8 +3,8 @@
 use std::io;
 use std::io::Write;
 
-fn read() -> i128{
-    let n: i128;
+fn read() -> i32{
+    let n: i32;
     let mut num = String::new();
     loop{
         num.clear();
@@ -30,11 +30,11 @@ fn read() -> i128{
 }
 
 fn main(){
-    let n: i128=read();
-    let mut rem: i128;
-    let mut rev: i128=0;
-    let mut tmp: i128=n;
-    
+    let n: i32=read();
+    let mut rem: i32;
+    let mut rev: i32=0;
+    let mut tmp: i32=n;
+
     loop{
         rev=rev+(tmp%10);
         rem=tmp/10;
@@ -49,11 +49,11 @@ fn main(){
             //println!("rev: {}", &rev);
         }
     }
-
+    println!("\nOutput: {}", &rev);
     if rev==n{
-        println!("\nInput is a palindrome!");
+        println!("Input is a palindrome!");
     }
     else {
-        println!("\nInput is not a palindrome!");
+        println!("Input is not a palindrome!");
     }
 }
