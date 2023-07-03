23e4d9486f88873a9d9a5f96cf63f1aa54991759
retrymp3 <retrymp492@gmail.com>
2023-07-03 10:36:46 +0530

    test program

--- CommonConcepts/general_stuff/src/test.rs
+++ CommonConcepts/general_stuff/src/test.rs
@@ -1,4 +1,21 @@
 fn main(){
 	let sum = -5.098789789; //All floating point datatypes are signed. By default it is 64 bits. 
 	println!("{}",sum);
+	println!("A");
+
+	print!("B");
+	print!("c");
+	println!("D");
+
+	eprint!("E");
+	eprint!("E");
+	eprint!("E");
+	eprint!("E\n");
+
+	println!("{subject} {verb} {object}",
+	object="the lazy dog",
+	subject="the quick brown fox",
+	verb="jumps over");
+
+	println!("My name is {0}, {1} {0}", "Bond", "James");
 }
