7c42d1ec54d0ad2f3883175ab42356da36ec5b47
retrymp3 <retrymp492@gmail.com>
2023-07-03 15:50:38 +0530

    Some stuff about if statements.

---
+++ CommonConcepts/general_stuff/src/ifconds.rs
@@ -0,0 +1,22 @@
+//This program won't compile, because in rust, the boolean value can't be determined by numbers/any other values.
+fn main(){
+    // let x=1;
+    // if x {
+    //     println!("True");
+    // }
+    // else{
+    //     println!("False");
+    // }
+
+
+}
+
+fn conds() -> &str{
+    let x=3;
+    if x<5 {
+        return "less";
+    }
+    else{
+        return "more";
+    }
+}
