149acba53945ca568277ddb4359a8d5834210c2c
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2023-07-07 14:57:28 +0530

    Update ifconds.rs

--- CommonConcepts/general_stuff/src/ifconds.rs
+++ CommonConcepts/general_stuff/src/ifconds.rs
@@ -1,16 +1,15 @@
 //This program won't compile, because in rust, the boolean value can't be determined by numbers/any other values.
-fn main(){
-    // let x=1;
-    // if x {
-    //     println!("True");
-    // }
-    // else{
-    //     println!("False");
-    // }
+// fn main(){
+//      let x=1;
+//      if x {
+//          println!("True");
+//      }
+//      else{
+//          println!("False");
+//      }
+// }
 
-
-}
-
+//This will compile.
 fn conds() -> &str{
     let x=3;
     if x<5 {
